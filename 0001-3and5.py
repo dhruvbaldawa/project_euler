b299bcb59f95642d6ed6f9db370eaabcc8c53d2b
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2011-12-30 14:34:18 +0530

    Solved the problem 2

--- 0001-3and5.py
+++ 0001-3and5.py
@@ -1,4 +1,10 @@
-import math
+"""
+Problem 1
+Add all the natural numbers below one thousand that are multiples of 3 or 5.
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 def bruteforce():
     sum_ = 0
     number = 1000
