6fd776f7c20d9ae8bb1fecee20a3c03f3add4384
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-04-23 08:16:35 +0530

    Solved Problem 46

---
+++ 0046-odd-composite-sum.py
@@ -0,0 +1,23 @@
+"""
+Problem 46
+
+It was proposed by Christian Goldbach that every odd composite
+number can be written as the sum of a prime and twice a square.
+
+9  = 7  + 2x1^2
+15 = 7  + 2x2^2
+21 = 3  + 2x3^2
+25 = 7  + 2x3^2
+27 = 19 + 2x2^2
+33 = 31 + 2x1^2
+
+It turns out that the conjecture was false.
+
+What is the smallest odd composite that cannot be written as
+the sum of a prime and twice a square?
+"""
+def traditional():
+    # Get Odd Composite numbers
+    pass
+
+print "Answer:", traditional()
