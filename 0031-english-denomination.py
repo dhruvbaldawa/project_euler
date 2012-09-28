9f1c5c1a343b7d632cefeb77b15b19c917637593
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-09-28 23:33:24 +0530

    Solved problem 31

--- 0031-english-denomination.py
+++ 0031-english-denomination.py
@@ -3,35 +3,23 @@ http://projecteuler.net/problem=31
 """
 count = 0
 
-def generate_denominations(base, coin, limit):
-    global count
-    if base + coin == limit:
-        count += 1
-    elif base + coin > limit:
-        return
+def generate_denominations(amount, coins):
+    # A successful find, add 1
+    if amount == 0:
+        return 1
+    # An unsuccessful find, add 0
+    elif amount < 0 or len(coins) < 1:
+        return 0
+    # Deduct the coin, and calculate and don't deduct and calculate with
+    # rest of the coins
     else:
-        generate_denominations(base+coin, 1, limit)
-        generate_denominations(base+coin, 2, limit)
-        generate_denominations(base+coin, 5, limit)
-        generate_denominations(base+coin, 10, limit)
-        generate_denominations(base+coin, 20, limit)
-        generate_denominations(base+coin, 50, limit)
-        generate_denominations(base+coin, 100, limit)
-        generate_denominations(base+coin, 200, limit)
-        
+        coin, rest = coins[0], coins[1:]
+        return generate_denominations(amount-coin, coins) + \
+            generate_denominations(amount, rest)
+           
 def main():
-    global count
-    limit = 200
-    
-    generate_denominations(0, 1, limit)
-    generate_denominations(0, 2, limit)
-    generate_denominations(0, 5, limit)
-    generate_denominations(0, 10, limit)
-    generate_denominations(0, 20, limit)
-    generate_denominations(0, 50, limit)
-    generate_denominations(0, 100, limit)
-    generate_denominations(0, 200, limit)
-    
-    print count
+    coins = [200, 100, 50, 20, 10, 5, 2, 1]
+    amount = 200
+    print generate_denominations(amount, coins)
     
 main()
